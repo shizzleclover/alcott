e50ac5c91f41d2e9c12b744dba4c6669b2c37c3f
shizzleclover <murewatajala@gmail.com>
2025-10-16 14:07:38 +0100

    push

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,6 +8,7 @@ export function Footer() {
         <div className="max-w-7xl mx-auto px-4 lg:px-12">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8">
             {/* Column 1: Alcott Logo and Description */}
+            
             <div className="lg:col-span-2">
               <div className="flex items-center mb-6">
                 <img src="/alcott-small.png" alt="alcott-small-logo"/>
